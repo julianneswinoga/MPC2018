1d051a2e5a04550d4fea958feaa2587385154276
Julianne Swinoga <julianneswinoga@gmail.com>
2018-02-03 17:13:40 -0500

    Tom multithreading

--- tom/tom.py
+++ tom/tom.py
@@ -1,4 +1,5 @@
 import math
+import threading
 
 nl = int(input())
 sl = [int(i) for i in input().split(" ")]
@@ -6,29 +7,51 @@ ac = 0
 ra = 0
 ob = 0
 eps = 0.0000001
+num_threads = 4
 
-for i in range(len(sl)):
-    for j in range(i + 1, len(sl)):
-        for k in range(j + 1, len(sl)):
-            a = sl[i]
-            b = sl[j]
-            c = sl[k]
+
+class TriangleThread(threading.Thread):
+    def __init__(self, triangles):
+        threading.Thread.__init__(self)
+        self.triangles = triangles
+        self.ra = 0
+        self.ob = 0
+        self.ac = 0
+
+    def run(self):
+        for t in self.triangles:
+            a, b, c = t[0], t[1], t[2]
             s = (a + b - c) * (a + c - b) * (c + b - a)
-
             if s < eps:
                 continue
-
             try:
                 C = math.acos((c ** 2 - a ** 2 - b ** 2) / (-2 * a * b))
                 B = math.asin((b * math.sin(C)) / c)
                 A = math.pi - B - C
                 if math.pi / 2 in [A, B, C]:
-                    ra += 1
+                    self.ra += 1
                 elif max([A, B, C]) > math.pi / 2:
-                    ob += 1
+                    self.ob += 1
                 else:
-                    ac += 1
+                    self.ac += 1
             except:
                 continue
 
-print(ac, ra, ob)
+
+all_permuations = []
+for i in range(len(sl)):
+    for j in range(i + 1, len(sl)):
+        for k in range(j + 1, len(sl)):
+            all_permuations.append([sl[i], sl[j], sl[k]])
+threads = []
+chunks = [all_permuations[x:x+num_threads] for x in range(0, len(all_permuations), num_threads)]
+for chunk in chunks:
+    threads.append(TriangleThread(chunk))
+
+for T in threads:
+    T.start()
+
+for T in threads:
+    T.join()
+
+print(sum([T.ac for T in threads]), sum([T.ra for T in threads]), sum([T.ob for T in threads]))
