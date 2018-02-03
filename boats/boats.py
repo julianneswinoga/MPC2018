cda9661204066c36bdbcb447aa8c97e2bd04c7eb
Julianne Swinoga <julianneswinoga@gmail.com>
2018-02-03 12:43:56 -0500

    slightly more optimized boats

--- boats/boats.py
+++ boats/boats.py
@@ -1,14 +1,17 @@
 D = int(input())
 B = int(input())
+wanted_dock = []
+for _ in range(B):
+    wanted_dock.append(int(input()))
+
 
 dock = [0] * D
 for di in range(B):
-    wanted_dock = int(input())
-    while wanted_dock > 0:
-        if not dock[wanted_dock - 1]:
-            dock[wanted_dock - 1] = 1
+    while wanted_dock[di] > 0:
+        if not dock[wanted_dock[di] - 1]:
+            dock[wanted_dock[di] - 1] = 1
             break
-        wanted_dock -= 1
+        wanted_dock[di] -= 1
     else:
         break
 
